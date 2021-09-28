e5d5a4acc0e2a009950d044bbd0f4a7fc8717545
sigvant <bnsviv@gmail.com>
2021-09-28 14:21:23 -0300

    final portal class

--- reactjstutorialvishwasl/src/App.js
+++ reactjstutorialvishwasl/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import FRParentInput from './components/FRParentInput';
+import PortalDemo from './components/PortalDemo';
 
 function App() {
   return (
     <div className="App">
-      <FRParentInput/>
+      <PortalDemo/>
     </div>
   );
 }
