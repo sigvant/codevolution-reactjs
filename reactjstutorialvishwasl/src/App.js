8c2cc5b405e42c13f7b140e55757e4e66b473d22
sigvant <bnsviv@gmail.com>
2021-09-28 08:30:04 -0300

    final refsinclass

--- reactjstutorialvishwasl/src/App.js
+++ reactjstutorialvishwasl/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-import RefsDemo from './components/RefsDemo';
+import FocusInput from './components/FocusInput';
+// import RefsDemo from './components/RefsDemo';
 
 function App() {
   return (
     <div className="App">
-      <RefsDemo/>
+      <FocusInput/>
     </div>
   );
 }
