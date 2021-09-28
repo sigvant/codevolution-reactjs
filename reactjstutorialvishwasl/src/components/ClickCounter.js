2546c528fd5ee8198609607b63227119a78d838a
sigvant <bnsviv@gmail.com>
2021-09-28 19:25:29 -0300

    end of HOC lessons

---
+++ reactjstutorialvishwasl/src/components/ClickCounter.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import UpdatedComponent from './withCounter'
+
+class ClickCounter extends Component {
+    
+    render() {
+        const { count, incrementCount } = this.props
+        return (
+            <div>
+                <button onClick={incrementCount}>
+                    {this.props.name} Clicked {count} Times
+                </button>
+            </div>
+        )
+    }
+}
+
+export default UpdatedComponent(ClickCounter, 5)
