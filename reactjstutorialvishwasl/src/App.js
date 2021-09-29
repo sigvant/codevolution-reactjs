b14ca5343b9b6539f31233d9b5866fa19c884c8a
sigvant <bnsviv@gmail.com>
2021-09-29 07:21:32 -0300

    render props with render as function

--- reactjstutorialvishwasl/src/App.js
+++ reactjstutorialvishwasl/src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
 import ClickCounter2 from './components/ClickCounter2';
+import Counter from './components/Counter';
 import HoverCounter2 from './components/HoverCounter2';
 
 function App() {
   return (
     <div className="App">
-      <ClickCounter2/>
-      <HoverCounter2/>
+      <Counter
+        render={(count, incrementCount) => (
+          <ClickCounter2 count={count} incrementCount={incrementCount} />
+        )}
+      />
+      <Counter
+        render={(count, incrementCount) => (
+          <HoverCounter2 count={count} incrementCount={incrementCount} />
+        )}
+      />
+
     </div>
   );
 }
