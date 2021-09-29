91cef71972188d5c72a46245272bce4c5932a943
sigvant <bnsviv@gmail.com>
2021-09-29 08:21:45 -0300

    end of context

--- reactjstutorialvishwasl/src/App.js
+++ reactjstutorialvishwasl/src/App.js
@@ -1,23 +1,13 @@
 import './App.css';
-import ClickCounter2 from './components/ClickCounter2';
-import Counter from './components/Counter';
-import HoverCounter2 from './components/HoverCounter2';
+import ComponentC from './components/ComponentC';
+import { UserProvider } from './components/userContext';
 
 function App() {
   return (
     <div className="App">
-      <Counter>
-        {(count, incrementCount) => (
-          <ClickCounter2 count={count} incrementCount={incrementCount} />
-        )}
-        {/* anything between component tags will be passed as children props */}
-      </Counter>
-      <Counter>
-        {(count, incrementCount) => (
-          <HoverCounter2 count={count} incrementCount={incrementCount} />
-        )}
-      </Counter>
-
+      <UserProvider value={'Vishwas'}>
+        <ComponentC/>
+      </UserProvider>
     </div>
   );
 }
